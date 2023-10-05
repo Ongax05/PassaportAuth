e6eefc12dab04fabb60b79d54670ee87d4303960
Jhosep <jhoseppulido1129@gmail.com>
2023-10-05 11:56:34 -0500

    feat: :construction: Google Auth In Progres

--- API/Program.cs
+++ API/Program.cs
@@ -1,20 +1,42 @@
+using Microsoft.EntityFrameworkCore;
+using Persistence.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddAuthentication().AddGoogle(googleOptions =>
+{
+    googleOptions.ClientId = builder.Configuration["Authentication:Google:ClientId"];
+    googleOptions.ClientSecret = builder.Configuration["Authentication:Google:ClientSecret"];
+});
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+// using (var scope = app.Services.CreateScope())
+// {
+//     var services = scope.ServiceProvider;
+//     var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+//     try
+//     {
+//         var context = services.GetRequiredService<PassaportAuthContext>();
+//         await context.Database.MigrateAsync();
+//     }
+//     catch (Exception ex)
+//     {
+//         var _logger = loggerFactory.CreateLogger<Program>();
+//         _logger.LogError(ex, "Ocurrio un error durante la migracion");
+//     }
+// }
+
 
 app.UseHttpsRedirection();
 
--- API/obj/Debug/net7.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net7.0/API.AssemblyInfo.cs
@@ -10,6 +10,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("9c6d7e41-0a01-477a-b86d-d59606919a2b")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
